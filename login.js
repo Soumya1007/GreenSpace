d99373ffcb348511769763f1cd058354b29c63d6
div8791 <div8791@gmail.com>
2022-10-02 15:09:43 +0530

    added calculator

---
+++ login.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+app.get("/",function(req,res){
+    res.sendFile("index.html");
+});
+app.post("/",function(req,res){
+    var username = req.body.uname;
+    var password = req.body.pass;
+    res.redirect('index.html');
+});
